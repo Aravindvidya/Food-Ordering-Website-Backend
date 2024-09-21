5d97cd96edcb875e99e24b90a481ace6cc9ce212
Aravind Sasidharan <158824483+Aravindvidya@users.noreply.github.com>
2024-09-21 10:57:07 +0200

    Delete config/db.js

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://aravindpadmam12:gdMsZgMqCXaaWPCe@cluster0.gyd0y.mongodb.net/food-delivery').then(()=>console.log("DB connected"))
-}

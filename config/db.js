75c3a9725f7089e1a9ececda47d854479c869c0c
Aravind Sasidharan <158824483+Aravindvidya@users.noreply.github.com>
2024-09-25 12:17:12 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 export const connectDB = async () => {
-    const mongoUrl = process.env.MONGO_URL; 
+    const mongoUrl = process.env.MONGO_URI; 
     await mongoose.connect(mongoUrl)
         .then(() => console.log("DB connected"))
         .catch((error) => console.error("DB connection error:", error));
